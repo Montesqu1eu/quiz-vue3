50a1dbabb1ede3ab6bda57a22e9bc4c95797a9a2
Ildar Shamsetdinov <ildar.golden@gmail.com>
2023-12-13 14:39:43 +0700

    first

---
+++ src/components/Questions.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+        <div class="bar"></div>
+        <div class="status">1 out of 3 questions answered</div>
+    </div>
+    <div
+      v-for="question in questions"
+      :key="question.q"
+      class="single-question"
+    >
+      <div class="question">
+        {{ question.q }}
+      </div>
+      <div class="answers">
+          <div
+            v-for="answer in question.answers"
+            :key="answer.text"
+            class="answer"
+          >
+            {{ answer.text }}
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Questions',
+  props: {
+    questions: {
+      type: Array,
+      default: () => [],
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
